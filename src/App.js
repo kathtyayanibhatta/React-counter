2be4f8d210a07bfdb6616064c9b100c3212648a7
Kathyayani Bhatta <kathyayani@Kathyayanis-MacBook-Pro.local>
2024-11-15 12:05:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,15 +11,32 @@ function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const date = new Date("june 21 2027");
+  const [step, setStep] = useState(1);
+  const date = new Date();
   date.setDate(date.getDate() + count);
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
-        <span>Count : {count}</span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step : {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count : {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
